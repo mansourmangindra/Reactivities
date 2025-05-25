754e854eff7867a8ea0e11f2325ebab61196f52f
mansourmangindra <mansourmangindra@gmail.com>
2025-05-25 09:08:59 +0800

    End of Section 4

---
+++ Application/Activities/Queries/GetActivityList.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using Domain;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using Persistence;
+
+namespace Application.Activities.Queries;
+
+public class GetActivityList
+{
+    public class Query : IRequest<List<Activity>> { }
+
+    public class Handler(AppDbContext context) : IRequestHandler<Query, List<Activity>>
+    {
+        public async Task<List<Activity>> Handle(Query request, CancellationToken cancellationToken)
+        {
+            return await context.Activities.ToListAsync(cancellationToken);
+        }
+    }
+
+}
